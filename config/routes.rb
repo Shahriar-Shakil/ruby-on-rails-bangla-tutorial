fd4798e13de60d441796974874e0cc12561d043f
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-15 01:45:18 +0600

    delete about controller using generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # get "about/index"
   # match "about/index", to: "about#index", via: :get
-  get "about", to: "about#index"
+  # get "about", to: "about#index"
 
-  # root "home#index"
+  root "home#index"
   # match "/", to: "home#index", via: :get
-  get "/", to: "home#index"
+  # get "/", to: "home#index"
 end
