9b051351816ae1117a11dcfdd18a86f5c723de17
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-16 00:35:29 +0600

    14 CRUD Part 1, Setup index page and show single book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,9 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
   def show
-    @params = { id: params[:id], name: params[:name] }
+    # @params = { id: params[:id], name: params[:name] }
+    @book =Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get "about", to: "home#about", as: "about"
   get "hello", to: "hello#index", as: "hello"
   get "books", to: "books#index"
-  get "book", to: "books#show"
+  # get "book", to: "books#show"
+  get "books/:id", to: "books#show", as: "book"
 end
