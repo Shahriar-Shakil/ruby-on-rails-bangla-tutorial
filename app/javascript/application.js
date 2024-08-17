a70b514fc42db2d23ccda6fc058f2efaa6b91aa0
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-17 23:29:22 +0600

    add bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// app/javascript/application.js
+
+// Configure your import map in config/importmap.rb
+// Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails";
+import "popper";
+import "bootstrap";
+import "controllers";
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+
+pin "popper", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.bundle.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
